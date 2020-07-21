7ba3b80652dddc51eed3009c369cae1ed6a6ddfa
rokcej <rokcej1997@gmail.com>
2020-07-21 03:39:22 +0200

    FPS counter overlay

--- src/main.js
+++ src/main.js
@@ -9,9 +9,14 @@ import * as RC from '/rendercore/src/RenderCore.js'
 
 class App {
 	constructor(canvas) {
+		// Canvas
 		this.canvas = canvas;
 
-		this.timer = { curr: 0, prev: 0, delta: 0};
+		// Animation timer
+		this.timer = { curr: 0, prev: 0, delta: 0 };
+		// FPS
+		this.fpsCount = 0;
+		this.fpsTime = 0;
 		
 		this.renderer = new RC.MeshRenderer(this.canvas, RC.WEBGL2);
 		this.renderer.clearColor = "#000000FF";
@@ -24,13 +29,21 @@ class App {
 
 		this.initScene();
 		this.initRenderQueue();
-		this.loadResourcesAndAnimate(); // TODO: Cleanup
+		this.resize();
+
+		window.addEventListener("resize", () => { this.resize(); }, false);
+
+		this.loadResources(
+			() => { window.requestAnimationFrame(() => { this.update(); }); }
+		);
 	}
 
 
 	initScene() {
-		// Main scene
-		this.camera = new RC.PerspectiveCamera(60, 16 / 9, 0.1, 1000);
+		/// Main scene
+		this.scene = new RC.Scene();
+
+		this.camera = new RC.PerspectiveCamera(60, this.canvas.width / this.canvas.height, 0.1, 1000);
 		this.camera.position = new RC.Vector3(0, 0.75, 4);
 		this.camera.lookAt(new RC.Vector3(0, 0, 0), new RC.Vector3(0, 1, 0));
 		
@@ -38,33 +51,31 @@ class App {
 		this.cameraManager.addFullOrbitCamera(this.camera, new RC.Vector3(0, 0, 0));
 		this.cameraManager.activeCamera = this.camera;
 
-		this.scene = new RC.Scene();
-
+		// Lights
 		this.dLight = new RC.DirectionalLight(new RC.Color("#FFFFFF"), 1.0);
 		this.dLight.position = new RC.Vector3(1.0, 0.5, 0.8);
 		this.pLight = new RC.PointLight(new RC.Color("#FFFFFF"), 1.0);
 		this.pLight.position = new RC.Vector3(-4.0, 10.0, -20.0);
 		this.aLight = new RC.AmbientLight(new RC.Color("#FFFFFF"), 0.05);
 
+        this.pLight.add(new RC.Cube(1.0, this.pLight.color));
+
 		this.scene.add(this.dLight);
 		this.scene.add(this.pLight);
 		this.scene.add(this.aLight);
 
+		// Plane
 		let plane = new RC.Quad({x: -32, y: 32}, {x: 32, y: -32}, new RC.MeshPhongMaterial());
 		plane.material.color = new RC.Color("#FFFFFF");
 		plane.material.color = new RC.Color("#FFFFFF");
 		plane.material.specular = new RC.Color("#FFFFFF");
 		plane.material.shininess = 1;
-		plane.material.lights = true;
 		plane.material.side = RC.FRONT_AND_BACK_SIDE;
-		//plane.material.shadingType = RC.SmoothShading;
-		plane.material.transparent = false;
-		//plane.material.opacity = 1.0;
 		plane.translateY(-4);
 		plane.rotateX(Math.PI * 0.5);
 		this.scene.add(plane);
 
-		// Post production scene
+		/// Post production scene
 		let waterShaderMaterial = new RC.CustomShaderMaterial("water");
 		waterShaderMaterial.color = new RC.Color(1.0, 1.0, 1.0);
 
@@ -119,7 +130,7 @@ class App {
 		this.renderQueue.pushRenderPass(this.waterRenderPass);
 	}
 
-	loadResourcesAndAnimate() {
+	loadResources(callback) {
 		this.manager = new RC.LoadingManager();
 		this.objLoader = new RC.ObjLoader(this.manager);
 		//this.imageLoader = new RC.ImageLoader(this.manager);
@@ -159,15 +170,25 @@ class App {
 				}
 			}
 
-			window.requestAnimationFrame(() => { this.update(); });
+			callback();
 		});
 	}
 
 	update() {
 		// Timer
 		this.timer.prev = this.timer.curr;
-		this.timer.curr = performance.now(); // Convert to seconds
+		this.timer.curr = performance.now();
 		this.timer.delta = this.timer.curr - this.timer.prev;
+
+		// FPS
+		++this.fpsCount;
+		if (this.timer.curr - this.fpsTime >= 1000) {
+			const fps = this.fpsCount * 1000 / (this.timer.curr - this.fpsTime);
+			document.getElementById("fps").innerHTML = Math.round(fps).toString();
+
+			this.fpsCount = 0;
+			this.fpsTime = this.timer.curr;
+		}
 
 		// Camera
 		const input = {
@@ -190,6 +211,20 @@ class App {
 	render() {
 		//this.renderer.render(this.scene, this.camera);
 		this.renderQueue.render();
+	}
+
+	resize() {
+		// Resize canvas
+		this.canvas.width  = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+	
+		// Update aspect ratio and viewport
+		this.camera.aspect = this.canvas.width / this.canvas.height;
+		this.renderer.updateViewport(this.canvas.width, this.canvas.height);
+	
+		// Update render passes
+		this.mainRenderPass.viewport = { width: this.canvas.width, height: this.canvas.height };
+		this.waterRenderPass.viewport = { width: this.canvas.width, height: this.canvas.height };
 	}
 }
 
